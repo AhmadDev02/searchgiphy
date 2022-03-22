09401ecbd951c418da5c6bdd403aa0650001258c
Ahmad Fajar Alfaravi <afajaralfaravi@gmail.com>
2022-03-22 19:24:02 +0700

    commit my tugas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+
+
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      
+      <section className="playlist">
+      <h1 className="App">PLAYLIST SONG SPORTIFY</h1>
+        <div className="login">
+          <form>
+          <img className="img" src="https://i.scdn.co/image/ab67616d0000b273e8b066f70c206551210d902b" alt="" />
+            <br/>
+            <div>
+                <label>Title:</label>
+                <select name="title" id="title">
+                <option >Select</option>
+                <option value="song1">Somebody To Love</option>
+                <option value="song2">Doing All Right</option>
+                <option value="song3">Killer Queen</option>
+                <option value="song4">I Want To Break Free</option>
+                </select>
+
+            </div>
+            <div>
+                <label>Artist:</label>
+                <select name="artist" id="artist">
+                <option >Select</option>
+                <option value="queen">Queen</option>
+                </select>
+            </div>
+            <br />
+            <div>
+                <button id="get-btn" class="tombol">Select</button>
+            </div>
+           
+          </form>
+        </div>
+
+
+
+        <div className="queue">
+          <div className="queue-cover">
+            <img src="https://i.scdn.co/image/ab67616d0000b273e8b066f70c206551210d902b" alt="" />
+            <i className="fas fa-pause"></i>
+          </div>
+          <p className="name">Bohemian Rhapsody (The Original Soundtrack)/
+            Queen</p>
+        </div>
+        <br />
+        <div className="queue">
+          <div className="queue-cover">
+            <img src="https://i.scdn.co/image/ab67616d0000b273e8b066f70c206551210d902b" alt="" />
+            <i className="fas fa-pause"></i>
+          </div>
+          <p className="name">
+            Bohemian Rhapsody (The Original Soundtrack)/
+            Queen
+          </p>
+          <p >Queen</p>
+        </div>
+        <br />
+        <div className="queue">
+          <div className="queue-cover">
+            <img src="https://i.scdn.co/image/ab67616d00004851e8b066f70c206551210d902b" alt="" />
+            <i className="fas fa-pause"></i>
+          </div>
+          <p className="name">Bohemian Rhapsody (The Original Soundtrack)/
+            Queen</p>
+        </div>
+         </section>
     </div>
   );
 }
-
 export default App;
